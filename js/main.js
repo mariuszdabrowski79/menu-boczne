d803a27d2bac003141b0d4176e5195ca78220eab
mariuszdabrowski79 <mariuszdabrowski79@op.pl>
2023-07-15 11:13:26 +0200

    Poprawione zadanie

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('.hamburger').click(function () {
         $('.main-menu').toggleClass('open');
     });
-    $('main-menu-item a').click(function (e) {
+    $('.main-menu-item a').click(function (e) {
         e.preventDefault();
         $('body').fadeOut(2000, function () {
             window.location.href = $(e.target).attr('href');
